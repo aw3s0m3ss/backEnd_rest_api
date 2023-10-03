550c759a81cd5c8c70adf889ae06c32d7f470de8
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-10-03 16:55:07 +0300

    first deplaoy

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+import router from './router';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(cors({
+    credentials:true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+const port = 8080;
+
+server.listen(port,()=>{
+    console.log(`active on http://localhost:${port}/`)
+});
+
+const MONGO_URL = "mongodb+srv://aw3s0m3ss:qqaa1234@cluster0.uuvbtyf.mongodb.net/";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error:Error)=>{
+    console.log(error)
+})
+
+app.use('/',router());
